adc0de35bb409822c5fbde5fc0c92cf65c60d47e
Malgorzata <bolek.malgorzata23@gmail.com>
2025-10-15 08:43:42 +0200

    modified:   myapp/polls/views.py

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -7,16 +7,15 @@ from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
+    output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(template.render(context, request))
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -27,11 +26,3 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
