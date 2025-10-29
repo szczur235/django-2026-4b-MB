df232016f3cc89b81fba13b3ba0c7e628e8d970b
Malgorzata <bolek.malgorzata23@gmail.com>
2025-10-29 08:24:53 +0100

    renamed:    myapp/polls/templates/detail.html -> myapp/polls/templates/polls/detail.html 	modified:   myapp/polls/templates/polls/index.html 	modified:   myapp/polls/urls.py 	modified:   myapp/polls/views.py

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -8,15 +8,12 @@ from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -25,4 +22,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
